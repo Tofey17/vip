8bc9b84e25941a0ea5ddb02e8cf196dd8ed825be
Tofey17 <154902093+Tofey17@users.noreply.github.com>
2024-03-14 12:16:14 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -40,6 +40,7 @@ bot_username = '@EEObot'
 bot_usernamee = '@A_MAN9300BOT'
 bot_usernameee = '@Ershkalibot'
 bot_usernameeee = '@Bellllen192BOT'
+bot_usernameeeee ='@g6u_bot'
 
 y = datetime.datetime.now().year
 m = datetime.datetime.now().month
@@ -63,7 +64,7 @@ time_bio = ["off"]
 
 async def join_channel():
     try:
-        await sython(JoinChannelRequest("@zzsszzz"))
+        await sython(JoinChannelRequest("@B_8_K"))
     except BaseException:
         pass
 
@@ -204,6 +205,47 @@ async def _(event):
                     bott = url.split('/')[-1]
                     await sython(ImportChatInviteRequest(bott))
                 msg2 = await sython.get_messages('@Ershkalibot', limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                
+            except:
+                await sython.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
+                break
+        await sython.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تجميع محمديون"))
+async def _(event):
+        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
+        channel_entity = await sython.get_entity(bot_usernameee)
+        await sython.send_message('@Ershkalibot', 'جاري التجميع بواسطة | 𝗦𝗢𝗨𝗥𝗖𝗘 𝗕𝗗𝗧𝗛𝗢𝗡')
+        channel_entity = await sython.get_entity(bot_usernameee)
+        await sython.send_message('@Ershkalibot', '/start')
+        await asyncio.sleep(5)
+        msg0 = await sython.get_messages('@g6u_bot', limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(5)
+        msg1 = await sython.get_messages('@g6u_bot', limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+
+            await asyncio.sleep(5)
+
+            list = await sython(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sython.send_message(event.chat_id, f"لايوجد قنوات  في البوت | SY")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await sython(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await sython(ImportChatInviteRequest(bott))
+                msg2 = await sython.get_messages('@g6u_bot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 
